6f78e20383061f64c3488bdb0a70111582e2088a
abipa <angger.binuko@gmail.com>
2015-12-22 18:32:23 +0700

    update api chat with authentication oauth

--- app/Modules/Chat/routes.php
+++ app/Modules/Chat/routes.php
@@ -7,8 +7,10 @@ Route::group(array('module' => 'Chat', 'namespace' => 'App\Modules\Chat\Controll
 });
 
 // API Service Route
-Route::group(array('prefix'=>'api/v1', 'namespace' => 'App\Modules\Chat\Controllers'), function(){
+Route::group(['prefix'=>'api/v1', 'namespace' => 'App\Modules\Chat\Controllers'], function(){
 
     Route::resource('chat','ChatController',array('except'=>array('create','edit')));
 
 });
+
+// Chat with oauth
