19d25286a0090a6c126e0e636a5ae4d39695bac4
abipa <angger.binuko@gmail.com>
2015-12-23 15:43:38 +0700

    update route api using versioning api/v1

--- app/Modules/User/routes.php
+++ app/Modules/User/routes.php
@@ -8,6 +8,7 @@ Route::group(array('module' => 'User', 'namespace' => 'App\Modules\User\Controll
 
 // API Service Route
 Route::group(array('prefix'=>'api/v1', 'namespace' => 'App\Modules\User\Controllers'), function(){
+    
     //Request::input('access_token', $access_token);
     Route::get('/user', ['middleware' => 'oauth', 'as' => 'api.user.index', 'uses' => 'UserController@index']);
     // api for insert new data user (register)
