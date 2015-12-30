10fa7a41a8550f0dcebb285ae0384059063c42f1
abipa <angger.binuko@gmail.com>
2015-12-30 17:20:11 +0700

    update route api user and chat

--- app/Modules/Chat/routes.php
+++ app/Modules/Chat/routes.php
@@ -2,16 +2,17 @@
 
 Route::group(array('module' => 'Chat', 'namespace' => 'App\Modules\Chat\Controllers'), function() {
 
+    // API Service Route
+    Route::group(['prefix'=>'api/v1'], function(){
+
+        // api for get data chat
+        Route::get('/chat', ['middleware' => 'oauth', 'as' => 'api.chat.index', 'uses' => 'ChatController@index']);
+        // api for insert data chat
+        Route::post('/chat', ['middleware' => 'oauth', 'as' => 'api.chat.store', 'uses' => 'ChatController@store']);
+
+    });
+
     Route::resource('Chat', 'ChatController');
     
 });
 
-// API Service Route
-Route::group(['prefix'=>'api/v1', 'namespace' => 'App\Modules\Chat\Controllers'], function(){
-
-    // api for get data chat
-    Route::get('/chat', ['middleware' => 'oauth', 'as' => 'api.chat.index', 'uses' => 'ChatController@index']);
-    // api for insert data chat
-    Route::post('/chat', ['middleware' => 'oauth', 'as' => 'api.chat.store', 'uses' => 'ChatController@store']);
-
-});
