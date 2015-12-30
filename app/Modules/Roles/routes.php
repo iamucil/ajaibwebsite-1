4165011d52825d4282509dd0d1eba90690f7ba5f
ucil619 <ecko.ucil@gmail.com>
2015-12-30 14:19:52 +0700

    update roles routes

--- app/Modules/Roles/routes.php
+++ app/Modules/Roles/routes.php
@@ -1,17 +1,14 @@
 <?php
 
 Route::group(array('module' => 'Roles', 'namespace' => 'App\Modules\Roles\Controllers'), function() {
-
+    Route::get('roles/generate', 'RolesController@generateRoles')->name('generate');
     Route::resource('roles', 'RolesController');
-    Route::get('/start', 'RolesController@generateRoles');
-
 });
 
-Route::group([
-    'prefix' => 'roles',
-    'module' => 'Role',
-    'as' => 'roles.',
-    'namespace' => 'App\Modules\Roles\Controllers'], function () {
-        Route::get('/start', 'RolesController@generateRoles')->name('start');
-        Route::post('/attach-role/{id}', 'RolesController@attachRole')->name('attach');
-});
+// Route::group([
+//     'prefix' => 'entrust',
+//     'module' => 'Role',
+//     'as' => 'roles::',
+//     'namespace' => 'App\Modules\Roles\Controllers'], function () {
+//         Route::post('/attach-role/{id}', 'RolesController@attachRole')->name('attach');
+// });
