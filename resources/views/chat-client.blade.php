b1ef5c1b6470a050c991761b897198ff3324442f
abipa <angger.binuko@gmail.com>
2015-12-30 17:51:01 +0700

    implementation  for testing chat client

--- resources/views/chat-client.blade.php
+++ resources/views/chat-client.blade.php
@@ -51,8 +51,20 @@
 <div class="container">
     <div class="content">
         <div class="title">Client Chat Feature</div>
-        <input type="text" id="chat_tf" value=""/>
-        <button id="send_bt" name="send">SEND</button>
+        <label>GET TOKEN HERE</label>
+        <div>
+            <input type="text" id="vercode" placeholder="verification code" size="40" value=""/>
+            <button id="get_token" name="get_token">TOKEN</button>
+        </div>
+        <div>
+            <input type="text" id="access_token" placeholder="access token" size="49" value="" readonly/>
+        </div>
+        <br/>
+        <label>LETS CHAT HERE</label>
+        <div>
+            <input type="text" id="chat_tf" placeholder="message" size="40" value=""/>
+            <button id="send_bt" name="send" >SEND &nbsp;</button>
+        </div>
         {{--<button id="req_bt" name="request">REQUEST CHAT</button>--}}
     </div>
 </div>
@@ -80,6 +92,32 @@
         return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
     }
 
+    // Get token
+    $('#get_token').click(function () {
+        var vercode = $('#vercode').val();
+        // get access token
+        $.ajax({
+            type: "POST",
+            url: "https://getajaib.local/api/v1/oauth/grant_access",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: JSON.stringify({
+                id:"f3d259ddd3ed8ff3843839b",
+                secret:"4c7f6f8fa93d59c45502c0ae8c4a95b",
+                code:vercode
+            }),
+            success: function (data, status, jqXHR) {
+                // success handler
+                $("#access_token").val(data['access_token']);
+            },
+            error: function (jqXHR, status) {
+                // error handler
+                console.log(jqXHR);
+                alert('fail' + status.code);
+            }
+        });
+    });
+
     var PUBNUB_demo = PUBNUB.init({
         publish_key: 'pub-c-20764d9e-b436-4776-b03a-adcae96c2a6b',
         subscribe_key: 'sub-c-6bad3874-9efa-11e5-baf7-02ee2ddab7fe',
@@ -96,8 +134,8 @@
 
     $('#send_bt').click(function () {
         var text = $('#chat_tf').val();
+        var access_token = $('#access_token').val();
         var datetime = "LastSync: " + new Date().today() + " @ " + new Date().timeNow();
-        var access_token = "RwpY6DHi7yWk4nXOrUvDA78FBoH0b13JGPTrFROh";
         PUBNUB_demo.publish({
             channel: 'OPERATOR',
             message: {
@@ -111,7 +149,7 @@
             }
         });
 
-        // get access token
+        // Send to API chat
         $.ajax({
             type: "POST",
             url: "https://getajaib.local/api/v1/chat",
