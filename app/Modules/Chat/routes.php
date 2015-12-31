2eede6b45cd55c12352918f9ffcff3a651b7eff4
abipa <angger.binuko@gmail.com>
2015-12-31 11:43:04 +0700

    remove resource in route chat

--- app/Modules/Chat/routes.php
+++ app/Modules/Chat/routes.php
@@ -1,9 +1,9 @@
 <?php
 
-Route::group(array('module' => 'Chat', 'namespace' => 'App\Modules\Chat\Controllers'), function() {
+Route::group(array('module' => 'Chat', 'namespace' => 'App\Modules\Chat\Controllers'), function () {
 
     // API Service Route
-    Route::group(['prefix'=>'api/v1'], function(){
+    Route::group(['prefix' => 'api/v1'], function () {
 
         // api for get data chat
         Route::get('/chat', ['middleware' => 'oauth', 'as' => 'api.chat.index', 'uses' => 'ChatController@index']);
@@ -11,6 +11,6 @@ Route::group(array('module' => 'Chat', 'namespace' => 'App\Modules\Chat\Controll
         Route::post('/chat', ['middleware' => 'oauth', 'as' => 'api.chat.store', 'uses' => 'ChatController@store']);
 
     });
-    
+
 });
 
