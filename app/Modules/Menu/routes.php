d19b2444524adf7345ae23d3906d5ca5dc617e57
ucil619 <ecko.ucil@gmail.com>
2016-01-05 22:21:12 +0700

    Update routing for menus

--- app/Modules/Menu/routes.php
+++ app/Modules/Menu/routes.php
@@ -1,7 +1,18 @@
 <?php
 
-Route::group(array('module' => 'Menu', 'namespace' => 'App\Modules\Menu\Controllers'), function() {
+Route::group(['module' => 'Menu', 'prefix' => 'dashboard', 'namespace' => 'App\Modules\Menu\Controllers', 'middleware' => ['auth', 'role:admin:root']], function() {
 
-    Route::resource('Menu', 'MenuController');
-    
-});	
+    Route::resource('menus', 'MenuController', [
+        'names' => [
+            'create'    => 'menus.create',
+            'index'     => 'menus.index',
+            'store'     => 'menus.store',
+            'edit'      => 'menus.edit',
+            'delete'    => 'menus.delete',
+            'update'    => 'menus.update',
+            'show'      => 'menus.show',
+            'destroy'   => 'menus.destroy',
+        ]
+    ]);
+
+});
